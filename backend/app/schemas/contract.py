01ba4baa9216cc5a300934baede393da6ac218fe
Bondarenko Mikhail <misha.bondarenko2018@yandex.ru>
2025-10-07 14:26:39 +0300

    пример для обновления договоров

--- backend/app/schemas/contract.py
+++ backend/app/schemas/contract.py
@@ -1,8 +1,23 @@
 from pydantic import BaseModel
-
+from typing import Optional
 
 class ContractGenerationResponse(BaseModel):
     counterparty_id: int
     file_path: str
     message: str
 
+
+class ContractDataSchema(BaseModel):
+    contract_number: Optional[int] = None
+    date: Optional[str] = None
+    director: Optional[str] = None
+    short_name: Optional[str] = None
+    legal_address: Optional[str] = None
+    inn: Optional[int] = None
+    kpp: Optional[int] = None
+    bank_account: Optional[int] = None
+    bank_name: Optional[str] = None
+    bank_corr: Optional[int] = None
+    bank_bik: Optional[int] = None
+    phone: Optional[str] = None
+    email: Optional[str] = None
